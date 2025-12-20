847ac45222b55b0cb638912ac103b706af4949dd
Oleg Ivanchenko <oleg.ivanchenko@iru.org>
2025-12-20 19:33:17 +0100

    Acknowledge unused parameter in `RegisterRoutes` by replacing `r` with `_` in developer module

--- modules/developer/module.go
+++ modules/developer/module.go
@@ -49,7 +49,7 @@ func (m *Module) Shutdown() error {
 }
 
 // RegisterRoutes registers public routes for the module.
-func (m *Module) RegisterRoutes(r chi.Router) {
+func (m *Module) RegisterRoutes(_ chi.Router) {
 	// No public routes for developer module
 }
 
